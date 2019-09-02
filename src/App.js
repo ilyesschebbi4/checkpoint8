7b19872504f13c426be16f1e33ac5c6b1fc9b79e
ilyess <ilyesschebbi@outlook.com>
2019-09-02 22:51:24 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import a from '../src/image/1.jpg'
+import b from '../src/image/2.jpg'
+import c from '../src/image/3.jpg'
+
+
+class App extends React.Component {
+state= {
+  Img : a,
+  Name:'Anas',
+Description:'Anas ya 8oul'
+}
+
+
+  render(){
+    return (<>
+    <button onClick={()=>{this.setState({
+      Img:a,
+      Name:'Anas',
+      Description:'Anas ya 8oul'
+    })}} >sarra</button>
+    <button onClick={()=>{this.setState({
+     Img:b ,
+     Name:'7ama',
+     Description:'7ama ya wa7ch'
+    })}} >harold</button>
+    <button onClick={()=>{this.setState({
+      Img:c,
+      Name:'Bro',
+      Description:'hahahahahahaha :D'
+    })}} >Daniel</button>
+    <div>
+      <h1 >{this.state.Name}</h1>
+    <img style={{width:"250px"}} src={this.state.Img}></img>
+    <p>{this.state.Description}</p>
+
+
+</div>
+    </>
+
+
+      );
+
+  }
+ 
+  
 }
 
 export default App;
